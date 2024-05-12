a60ace4bbceec8155b6cad9b2ef6256b55998f3d
NadinKonst <nadikon@gmail.com>
2024-05-12 21:52:35 +0700

    smth about trending

--- src/trending.js
+++ src/trending.js
@@ -14,9 +14,8 @@ async function getTrending(url) {
     const respData = await resp.json();
     createTrendingMoviesSection(respData);
   } catch (error) {
-    (document.getElementById("error").textContent = "Error trending movies:"),
-      error;
-    document.getElementById("result").textContent = "";
+    document.getElementById("error").textContent =
+      "Error trending movies: " + error;
   }
 }
 
@@ -44,110 +43,27 @@ function createTrendingMoviesSection(data) {
     }</p>`;
 
     // modal
-    const playButton = document.createElement("button");
-    playButton.classList.add("playButton");
-    playButton.textContent = "More info";
+    // const playButton = document.createElement("button");
+    // playButton.classList.add("playButton");
+    // playButton.textContent = "More info";
 
-    movieCard.addEventListener("mouseenter", () => {
-      playButton.style.display = "block";
-    });
+    // movieCard.addEventListener("mouseenter", () => {
+    //   playButton.style.display = "block";
+    // });
 
-    movieCard.addEventListener("mouseleave", () => {
-      playButton.style.display = "none";
-    });
-
-    playButton.addEventListener("click", () => {
-      openModal(movie);
-      disableScroll();
-    });
+    // movieCard.addEventListener("mouseleave", () => {
+    //   playButton.style.display = "none";
+    // });
     // modal
 
     movieCard.appendChild(movieImg);
     movieCard.appendChild(infoElement);
     trendingMoviesContainer.appendChild(movieCard);
-    movieCard.appendChild(playButton);
+    // movieCard.appendChild(playButton);
   });
 }
 
 // modal
-const modalElement = document.createElement("div");
-modalElement.classList.add("modal-backdrop");
-
-async function openModal(movie) {
-  const id = movie.kinopoiskId;
-
-  const res = await fetch(api_url_trending + id, {
-    headers: {
-      "X-API-KEY": "475b780a-eb7a-4f03-ab2c-288319493865",
-      "Content-Type": "application/json",
-    },
-  });
-  const resData = await res.json();
-
-  const modalElement = document.createElement("div");
-  modalElement.classList.add("modal-backdrop");
-  const modalContent = document.createElement("div");
-  modalContent.classList.add("modal-content");
-
-  const movieImg = document.createElement("img");
-  movieImg.classList.add("modal-movie-img");
-  movieImg.src = resData.posterUrl;
-  movieImg.alt = resData.nameOriginal;
-
-  const movieTitle = document.createElement("h2");
-  movieTitle.classList.add("modal-title");
-  movieTitle.textContent = resData.nameOriginal;
-
-  const movieInfo = document.createElement("div");
-  movieInfo.classList.add("movieInfo");
-
-  const movieYear = document.createElement("p");
-  movieYear.classList.add("modal-year");
-  movieYear.textContent = resData.year;
-
-  const movieGenre = document.createElement("p");
-  movieGenre.classList.add("modal-genre");
-  movieGenre.textContent = `Жанр: ${resData.genres
-    .map((el) => el.genre)
-    .join(", ")}`;
-
-  const movieDescription = document.createElement("p");
-  movieDescription.classList.add("modal-description");
-  movieDescription.textContent = resData.description;
-
-  const closeButton = document.createElement("button");
-  closeButton.classList.add("closeButton");
-  closeButton.textContent = "x";
-
-  modalContent.appendChild(movieImg);
-  modalContent.appendChild(movieTitle);
-  modalContent.appendChild(movieYear);
-  modalContent.appendChild(movieGenre);
-  modalContent.appendChild(movieDescription);
-  modalContent.appendChild(closeButton);
-  modalElement.appendChild(modalContent);
-  wrapperMain.appendChild(modalElement);
-
-  const closeBtn = document.querySelector(".closeButton");
-  closeBtn.addEventListener("click", () => closeModal());
-}
-
-function closeModal() {
-  const modalBackdrop = document.querySelector(".modal-backdrop");
-  const wrapperMain = document.querySelector(".wrapper-main");
-  wrapperMain.removeChild(modalBackdrop);
-}
-
-window.addEventListener("click", (e) => {
-  if (e.target != modalElement) {
-    closeModal();
-  }
-});
-window.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    closeModal();
-  }
-});
 
 // modal
 
