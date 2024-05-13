c0b5c61cb9ce5561ea4638d53319e13339214564
VitOu666 <lecielvert777@gmail.com>
2024-05-13 19:06:12 +0100

    profile 2

--- profile-page.js
+++ profile-page.js
@@ -1,2 +1,12 @@
-const changeAvatar = document.querySelector(".divBut");
-changeAvatar.addEventListener("mouseover")
+const changeAvatarButton = document.querySelector(".divBut");
+const imgAvatar = document.querySelector(".cardAvatar");
+imgAvatar.addEventListener("mouseover", () => {
+    changeAvatarButton.classList.remove("hide");
+});
+imgAvatar.addEventListener("mouseout", () => {
+    changeAvatarButton.classList.add("hide");
+});
+
+const now = new Date();
+const time = document.getElementById("time");
+time.innerHTML = "Current time: " + now.toString();
